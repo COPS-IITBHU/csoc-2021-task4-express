449de8e0736eae83b2f5cf39f35b9f65b383bde3
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-07-16 17:48:28 +0530

    added schema

--- models/book.js
+++ models/book.js
@@ -4,5 +4,33 @@ var bookSchema=new mongoose.Schema({
 	/*TODO: DEFINE the following attributes-
     title, genre, author, description, rating (out of 5), mrp, available_copies(instances).
      */
+    title: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    mrp: {
+        type: Decimal128,
+        required: true
+    },
+    available_copies: {
+        type: Number,
+        default: 0
+    }
 })
 module.exports=mongoose.model("Book",bookSchema);
