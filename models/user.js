c4ec6f15e23964cc08231ae72324521849220989
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-07-16 18:20:10 +0530

    added userSchema

--- models/user.js
+++ models/user.js
@@ -4,11 +4,21 @@ var	passportLocal=require("passport-local-mongoose");
 var userSchema=new mongoose.Schema({
 
 	//TODO: DEFINE USERNAME AND PASSSWORD ATTRIBUTES
+    username:{
+        type: String,
+        required: true,
+    },
 
+    password: {
+        type: String,
+        required: true,
+    },
 
-    loaned_books:[
-        //TODO: embed reference to id's of book copies loaned by this particular user in this array
-    ]
+    loaned_books://TODO: embed reference to id's of book copies loaned by this particular user in this array
+    {
+        type: [ObjectId],
+        default: [],
+    }
 })
 userSchema.plugin(passportLocal);
 module.exports=mongoose.model("User",userSchema);
