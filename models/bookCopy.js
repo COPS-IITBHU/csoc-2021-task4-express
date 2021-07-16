85a0a5eec0ab4438a1b2045cf26d9e408e332d42
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-07-16 18:10:48 +0530

    added schema

--- models/bookCopy.js
+++ models/bookCopy.js
@@ -3,8 +3,20 @@ var mongoose=require("mongoose");
 var bookCopySchema=new mongoose.Schema({
 //TODO: DEFINE the following attributes-
  book:  //embed reference to id of book of which its a copy
- status: //TRUE IF AVAILABLE TO BE ISSUED, ELSE FALSE 
- borrow_data: //date when book was borrowed
- borrower: //embed reference to id of user who has borrowed it 
+ {   
+     type: ObjectId,
+     required: true
+ }, 
+ status: {
+     type: Boolean,
+     required: true
+ },
+ //TRUE IF AVAILABLE TO BE ISSUED, ELSE FALSE 
+ borrow_data: {
+     type: Date
+ }, //date when book was borrowed
+ borrower:{
+     type: ObjectId,
+ } //embed reference to id of user who has borrowed it 
 })
 module.exports=mongoose.model("Bookcopy",bookCopySchema);
