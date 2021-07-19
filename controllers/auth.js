0e2898836897efa9e2336c2c2831e0dd7b9e02d1
Dev Raj R <dev.raj.r1729@gmail.com>
2021-07-19 19:11:01 +0530

    added login and register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,42 @@
+const passport = require('passport');
+const User = require('../models/user');
+
 var getLogin = (req, res) => {
   //TODO: render login page
+  res.render('login',{title:'Login',message:req.query.message})
 };
 
-var postLogin = (req, res) => {
+//  (req, res) => {
   // TODO: authenticate using passport
   //On successful authentication, redirect to next page
-};
+  var postLogin = passport.authenticate('local',{
+    successRedirect : '/',
+    failureRedirect : '/login?message=Invalid+Username+or+Password'
+  });
+
 
 var logout = (req, res) => {
   // TODO: write code to logout user and redirect back to the page
+
 };
 
 var getRegister = (req, res) => {
   // TODO: render register page
+  res.render('register',{title:'Register'});
 };
 
 var postRegister = (req, res) => {
   // TODO: Register user to User db using passport
   //On successful authentication, redirect to next page
+  let newUser = new User({username: req.body.username});
+  User.register(newUser,req.body.password,(err,user)=>{
+    if (err) {
+      console.log(err);
+      res.render('register',{title:'Register',message:err.message});
+    }
+    console.log(user);
+    res.send('hello');
+  })
 };
 
 module.exports = {
