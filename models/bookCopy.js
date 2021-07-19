f5e7cda11fbfc93d7e50e60ae9a304a2410750d2
Dev Raj R <dev.raj.r1729@gmail.com>
2021-07-19 19:44:44 +0530

    removed validator

--- models/bookCopy.js
+++ models/bookCopy.js
@@ -4,7 +4,7 @@ var bookCopySchema=new mongoose.Schema({
 //TODO: DEFINE the following attributes-
  book:  //embed reference to id of book of which its a copy
  {   
-     type: ObjectId,
+     type: mongoose.Schema.Types.ObjectId,
      required: true
  }, 
  status: {
@@ -14,15 +14,15 @@ var bookCopySchema=new mongoose.Schema({
  //TRUE IF AVAILABLE TO BE ISSUED, ELSE FALSE 
  borrow_data: {
      type: Date,
-     required: function() {
-         return !this.status;
-     }
+    //  required: function() {
+    //      return !this.status;
+    //  }
  }, //date when book was borrowed
  borrower:{
-     type: ObjectId,
-     required: function() {
-         return !this.status;
-     }
+     type: mongoose.Schema.Types.ObjectId,
+    //  required: function() {
+    //      return !this.status;
+    //  }
  } //embed reference to id of user who has borrowed it 
 })
 module.exports=mongoose.model("Bookcopy",bookCopySchema);
